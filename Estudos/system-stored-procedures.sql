47f116922c86641514267c04b0439c1b4d14ff9d
Vitor Tatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2023-11-06 14:06:14 -0300

    Create system-stored-procedures.sql

---
+++ Estudos/system-stored-procedures.sql
@@ -0,0 +1,8 @@
+-- STORAGE PROCEDURES PARA VERIFICAR INFORMAÇÕES DAS TABELAS DO SISTEMA
+
+SP_COLUMNS SA1020
+
+SP_HELP SA1020
+
+-- HÁ UMA LISTA DE SYSTEM STORED PROCEDURES ATRAVÉS DO LINK ABAIXO 
+https://learn.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/system-stored-procedures-transact-sql?view=sql-server-ver16
